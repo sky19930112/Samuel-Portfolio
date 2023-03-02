89aedd284ba66c2132edbe4e810cbbcc3da68c8d
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2023-03-02 08:50:07 -0600

    starting

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
+
+function App() {
+ 
+}
+
+export default App;
