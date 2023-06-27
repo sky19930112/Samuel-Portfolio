c5a036c661b823a7a82d4429262b521c06bc4f38
sky19930112 <sky19930112@gmail.com>
2023-06-26 23:16:25 -0500

    getting nav bar works

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -17,25 +17,49 @@ const Navbar = () => {
           
 
             <ul className='hidden md:flex'>
-                <li className='py-7 text-5xl'>Home</li>
-                <li className='py-7 text-5xl'>About</li>
-                <li className='py-7 text-5xl'>Projects</li>
-                <li className='py-7 text-5xl'>Resume</li>
-                <li className='py-7 text-5xl'>Contact</li>
+                <li>
+                    <Link to='home' smooth={true} duration={500}>Home</Link>
+                </li>
+                <li>
+                    <Link to='about' smooth={true} duration={500}>About</Link>
+                </li>
+                <li>
+                    <Link to='skills' smooth={true} duration={500}>Skills</Link>
+                </li>
+                <li> 
+                    <Link to='work' smooth={true} duration={500}>Works</Link>
+                </li>
+                <li>
+                    <Link to='contact' smooth={true} duration={500}>Contact</Link>
+                </li>
             </ul>
 
 
             <div onClick={handleClick} className='md:hidden z-10'>
-             <FaBars />
+                {!nav ? <FaBars /> : <FaTimes />}
             </div>
 
 
             <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center text-gray-50'}>
-                <li className='py-7 text-5xl'>Home</li>
-                <li className='py-7 text-5xl'>About</li>
-                <li className='py-7 text-5xl'>Projects</li>
-                <li className='py-7 text-5xl'>Resume</li>
-                <li className='py-7 text-5xl'>Contact</li>
+                <li className='py-7 text-5xl'>
+                    <Link onClick={handleClick} to='home' smooth={true} duration={500}>Home</Link>
+                </li>
+                <li className='py-7 text-5xl'>
+                    {' '}
+                    <Link onClick={handleClick} to='about' smooth={true} duration={500}>About</Link>
+                </li>
+                <li className='py-7 text-5xl'>
+                    {' '}
+                    <Link onClick={handleClick} to='skills' smooth={true} duration={500}>Skills</Link>
+                </li>
+                <li className='py-7 text-5xl'>
+                    {' '}
+                    <Link onClick={handleClick} to='work' smooth={true} duration={500}>Work</Link>
+                </li>
+                <li className='py-7 text-5xl'>
+                    {' '}
+                    <Link onClick={handleClick} to='contact' smooth={true} duration={500}>Contact</Link>
+                </li>
             </ul>
 
 
