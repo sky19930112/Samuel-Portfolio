734225d06c9cd911f48a4363064fe94b9807f31c
sky19930112 <sky19930112@gmail.com>
2023-11-21 10:36:34 -0600

    add on project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import HTML from "./HTML.jpg"
-import Profolio from "./Profolio.jpg"
+import MusicPage from "./MusicPage.png"
 import PopQuiz from "./PopQuiz.jpg"
 import Scheduler from "./Scheduler.jpg"
 import PasswordGenerate from "./PasswordGenerate.jpg"
@@ -18,7 +17,7 @@ function Projects() {
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
        
             <div>
-              <img src={Profolio} className="photo" />
+              <img src={MusicPage} className="photo" />
               <div class="">
                 <a href="https://github.com/sky19930112/MusicTheory">
                   <button className="text-center rounded-lg px-4 py-3 m-2
@@ -53,6 +52,20 @@ function Projects() {
                        bg-white text-gray-700 font-bold text-lg">GitHub</button>
                 </a>
                 <a href="https://sky19930112.github.io/DailyScheduler/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg">Deployed Website</button>
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <img src={PasswordGenerate} className="photo" />
+              <div class="">
+                <a href="https://github.com/sky19930112/password-generator">
+                  <button className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg">GitHub</button>
+                </a>
+                <a href="https://sky19930112.github.io/password-generator/">
                   <button className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg">Deployed Website</button>
                 </a>
