5015eafc8ac5f523a1fcf0d6d83a07be9f0e5e7f
sky19930112 <sky19930112@gmail.com>
2023-12-11 14:37:44 -0600

    change BG color

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div name='about' className='w-full h-screen bg-black text-gray-300'>
+        <div name='about' className='w-full h-screen bg-[#0e2779] text-gray-300'>
             <div className='flex flex-col justify-center items-center w-full h-full'>
                 <div className='max-w-[1000px] w-full px-4 grid-col2 gap-8'>
                     <div className='sm:text-right pd-8 pl-4'>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#0e2779]'>
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
             <p className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>My name is Samuel Hsu</p>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>
