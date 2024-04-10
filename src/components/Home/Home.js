a4b8b2c8a348e9593107640d72272c2ee6797395
sky19930112 <sky19930112@gmail.com>
2024-04-10 15:59:11 -0500

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import samuel from './background.jpg'
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0e2779]'>
+    <div name='home' className='w-full h-screen bg-black' style={{ backgroundImage: `url(${samuel})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPositionX: '75%',}}>
       <div className='max-w-[1000px] mx-auto px-12 flex flex-col justify-center h-full'>
-            <p className='text-4xl sm:text-6xl font-bold text-[#c1cdf1]'>Samuel Hsu</p>
+            <p className='text-4xl sm:text-6xl font-bold text-[#c1cdf1] italic'>Samuel Hsu</p>
             <p className='text-[#c1cdf1] py-2 max-w-[500px] sm:text-2xl'>A Full Stack Web Developer</p>
-
             {/*
             <div>
               <button className='text-white group border-2 ox-6 py-3 my-2 flex items-center hover:bg-red-800'>View Works 
@@ -15,7 +15,6 @@ const Home = () => {
               </button>
             </div>
             */}
-
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50'>
             <div>
-                <h1 style={{width: '100px'}}>Samuel Hsu</h1>
+                <h1 class="text-xl "style={{width: '100px'}}>Samuel Hsu</h1>
             </div>
 
           
