63ff0d5cea7a707b1e7867a9ef661c303f4d3f7b
sky19930112 <sky19930112@gmail.com>
2024-12-16 18:53:36 -0600

    update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,28 @@ import samuel from './background.jpg'
 const Home = () => {
   return (
     <div name='home' className='w-full h-screen bg-black' style={{ backgroundImage: `url(${samuel})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', backgroundPositionX: '75%',}}>
-      <div className='max-w-[1000px] mx-auto px-12 flex flex-col justify-center h-full'>
-            <p className='text-4xl sm:text-6xl font-bold text-[#c1cdf1] italic'>Samuel Hsu</p>
-            <p className='text-[#c1cdf1] py-2 max-w-[500px] sm:text-2xl'>A Full Stack Web Developer</p>
+      <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
+        
+        <h1 className="text-5xl font-bold mb-8 relative group cursor-pointer">
+          <span className="relative z-10 transition-colors duration-300 group-hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 text-[#c1cdf1]">
+            Samuel Hsu
+          </span>
+          <span className="absolute inset-0 transform scale-x-0 transition-transform duration-300 origin-left group-hover:scale-x-100"></span>
+        </h1>
+
+        <p className="text-2xl mb-6 relative group cursor-pointer ">
+          <span className="relative z-10 transition-all duration-300 group-hover:text-blue-600 text-[#c1cdf1]">
+            A Full Stack Web Developer
+          </span>
+          <span className="absolute bottom-0 left-0 w-4/12 h-0.5 bg-blue-300 transform scale-x-0 transition-transform duration-300 origin-left group-hover:scale-x-100"></span>
+        </p>
+
+        <p className="text-xl relative group cursor-pointer overflow-hidden">
+          <span className="relative z-10 transition-all duration-150 text-[#c1cdf1] group-hover:text-blue-600">
+            Turning Ideas into Interactive Experiences
+          </span>
+          <span className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 transform translate-y-full transition-transform duration-150 group-hover:translate-y-0 max-w-sm"></span>
+        </p>
             {/*
             <div>
               <button className='text-white group border-2 ox-6 py-3 my-2 flex items-center hover:bg-red-800'>View Works 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import logo from './logo.png'
+import './Navbar.css';
 
 
 const Navbar = () => {
@@ -13,26 +14,23 @@ const Navbar = () => {
 
     return (
         <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-50'>
-            <div>
+            <div className='flex items-center'>
                 <img src={logo} style={{ width: '90px', height: '80px' }}/>
             </div>
-
-          
-
-            <ul className='hidden md:flex'>
-                <li>
+            <ul className='hidden md:flex text-emerald-400 text-lg font-medium transition-all duration-300 ease-in-out space-x-6 absolute left-1/2 -translate-x-1/2'>
+                <li className=' hover:text-pink-500 hover:scale-110'>
                     <Link to='/' smooth={true} duration={500}>Home</Link>
                 </li>
-                <li>
+                <li className=' hover:text-pink-500 hover:scale-110'>
                     <Link to='/about' smooth={true} duration={500}>About</Link>
                 </li>
-                <li>
+                <li className='  hover:text-pink-500 hover:scale-110'>
                     <Link to='/skills' smooth={true} duration={500}>Skills</Link>
                 </li>
-                <li> 
+                <li className='  hover:text-pink-500 hover:scale-110'> 
                     <Link to='/Projects' smooth={true} duration={500}>Projects</Link>
                 </li>
-                <li>
+                <li className='  hover:text-pink-500 hover:scale-110'>
                     <Link to='contact' smooth={true} duration={500}>Contact</Link>
                 </li>
             </ul>
