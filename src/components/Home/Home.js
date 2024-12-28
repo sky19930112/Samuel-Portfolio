01acae3ff50452a1d6febedc7c65b7d502d126ce
sky19930112 <sky19930112@gmail.com>
2024-12-28 13:10:26 -0600

    add on my photo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect }  from 'react'
-import {motion, useMotionValue, useTransform} from 'framer-motion'
+import {motion, useMotionValue, useTransform, useAnimation} from 'framer-motion'
 import './Home.css'
+import Samuel from './MyHead.jpg'
 
 const Home = () => {
   const mouseX = useMotionValue(0);
@@ -14,6 +15,27 @@ const Home = () => {
 
   const ideasX = useTransform(mouseX, (value) => value * 0.08);
   const ideasY = useTransform(mouseY, (value) => value * 0.08);
+
+  const randomControls = useAnimation();
+
+  useEffect(() => {
+    let isMounted = true; 
+    const animateRandom = async () => {
+      while (isMounted) {
+        await randomControls.start({
+          x: Math.random() * 200 - 100, 
+          y: Math.random() * 200 - 100,
+          transition: { duration: 2, ease: 'easeInOut' },
+        });
+      }
+    };
+    animateRandom();
+  
+    return () => {
+      isMounted = false;
+    };
+  }, [randomControls]);
+
 
   useEffect(() => {
     const handleMouseMove = (event) => {
@@ -41,8 +63,14 @@ const Home = () => {
     data-theme="light"
     >
       
-      <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
-        
+    <motion.img
+            src={Samuel}
+            alt="Samuel Background"
+            className="absolute top-1/2 right-1/4 w-40 h-40 opacity-50 pointer-events-none transform -translate-y-1/2"
+            animate={randomControls}
+          />
+
+      <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>        
         <motion.h1 
         className="text-8xl font-bold mb-8 relative group cursor-pointer"
         style={{ x: samuelX, y: samuelY }}
