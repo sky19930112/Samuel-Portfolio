31d1dc6c77c21301f90104286571b851e0467079
sky19930112 <sky19930112@gmail.com>
2024-12-28 12:09:42 -0600

    add reactions at home page with framer motion

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import {motion} from 'framer-motion'
+
 import './Home.css'
 
 const Home = () => {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+  const calculateOffset = (factor) => ({
+    x: (mousePosition.x - window.innerWidth / 2) * factor,
+    y: (mousePosition.y - window.innerHeight / 2) * factor,
+  });
+
   return (
     <motion.div 
     initial={{width:0}} 
@@ -15,33 +34,35 @@ const Home = () => {
       
       <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center h-full'>
         
-        <h1 className="text-8xl font-bold mb-8 relative group cursor-pointer">
+        <motion.h1 
+        className="text-8xl font-bold mb-8 relative group cursor-pointer"
+        style={{ translateX: calculateOffset(0.02).x, translateY: calculateOffset(0.02).y }}
+        >
           <span className="relative z-10 transition-colors duration-300 group-hover:text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ">
             Samuel Hsu
           </span>
           <span className="absolute inset-0 transform scale-x-0 transition-transform duration-300 origin-left group-hover:scale-x-100"></span>
-        </h1>
+        </motion.h1>
 
-        <p className="text-4xl mb-6 relative group cursor-pointer ">
+        <motion.p 
+        className="text-4xl mb-6 relative group cursor-pointer"
+        style={{ translateX: calculateOffset(0.04).x, translateY: calculateOffset(0.04).y }}
+        >
           <span className="relative z-10 transition-all duration-300 group-hover:text-blue-600 ">
             A Full Stack Web Developer
           </span>
           <span className="absolute bottom-0 left-0 w-4/12 h-0.5 bg-blue-300 transform scale-x-0 transition-transform duration-300 origin-left group-hover:scale-x-100"></span>
-        </p>
+        </motion.p>
 
-        <p className="text-3xl relative group cursor-pointer overflow-hidden">
+        <motion.p 
+        className="text-3xl relative group cursor-pointer overflow-hidden"
+        style={{ translateX: calculateOffset(0.06).x, translateY: calculateOffset(0.06).y }}
+        >
           <span className="relative z-10 transition-all duration-150  group-hover:text-blue-600">
             Turning Ideas into Interactive Experiences
           </span>
           <span className="absolute inset-0 bg-gradient-to-r from-green-400 to-blue-500 transform translate-y-full transition-transform duration-150 group-hover:translate-y-0 max-w-lg"></span>
-        </p>
-            {/*
-            <div>
-              <button className='text-white group border-2 ox-6 py-3 my-2 flex items-center hover:bg-red-800'>View Works 
-              <div className='group-hover:rotate-90 duration-300'><HiArrowNarrowRight className='ml-3' /></div>
-              </button>
-            </div>
-            */}
+        </motion.p>
       </div>
     </motion.div>
  
